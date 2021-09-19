6eb858a4295d594ea733dd2419c0bc9cc3d3485a
LanaBliz <90935983+LanaBliz@users.noreply.github.com>
2021-09-19 10:11:55 -0400

    created locaors folder

---
+++ src/main/java/Locators.java
@@ -0,0 +1,5 @@
+import org.openqa.selenium.By;
+
+public class Locators {
+    public static final By LINK_MEDIA = By.cssSelector("a[href='https://romanceabroad.com/media/index']");
+}
--- src/test/java/PhotoTest.java
+++ src/test/java/PhotoTest.java
@@ -6,11 +6,11 @@ public class PhotoTest extends BaseUI{
 
     String currentUrl;
     String expectedUrlMedia = "https://romanceabroad.com/media/index";
-    By LINK_MEDIA = By.cssSelector("a[href='https://romanceabroad.com/media/index']");
+
 
     @Test
     public void testPhoto(){
-        driver.findElement(LINK_MEDIA).click();
+        driver.findElement(Locators.LINK_MEDIA).click();
         currentUrl = driver.getCurrentUrl();
         System.out.println(currentUrl);
         Assert.assertEquals(currentUrl, expectedUrlMedia);
